d73a52faa95894749a465e101e653099cfdd8b12
BlueberryF11 <161259489+BlueberryF11@users.noreply.github.com>
2026-06-29 20:40:27 -0400

    Set base path to '/pack-essentials/'
    Update base path for deployment.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vitejs.dev
 export default defineConfig({
+  base: '/pack-essentials/',
   plugins: [react()],
 })
